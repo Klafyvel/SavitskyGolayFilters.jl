de359d472d7ec20077a4c3a597e9857ac14c9a44
Hugo Levy-Falk <klafyvel@gmail.com>
2020-12-17 10:46:31 +0100

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.10

---
+++ src/SavitskyGolayFilters.jl
@@ -0,0 +1,5 @@
+module SavitskyGolayFilters
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using SavitskyGolayFilters
+using Test
+
+@testset "SavitskyGolayFilters.jl" begin
+    # Write your tests here.
+end
